1f25a4ce9c573a05dd159d15555f0d2481804e9a
Kiril krasnozhon <kiril.krasnozhon@cleveroad.com>
2024-12-16 15:23:17 +0200

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,17 @@
 module.exports = {
   siteMetadata: {
     title: `gatsby-app`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.yourdomain.tld`,
   },
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: [
+    "gatsby-plugin-postcss",
+    "gatsby-transformer-remark",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+  ],
 };
